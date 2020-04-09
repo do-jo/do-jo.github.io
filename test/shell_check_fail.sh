28689de243013a8ed822c95f0219ed9f240861e5
Johnny Do <johnny@Johnnys-MacBook-Pro.local>
2020-04-09 07:38:31 -0700

    test git action

---
+++ test/shell_check_fail.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [1 -lt 10]
+then
+    echo "This should not print. See shellcheck results."
+fi
---
+++ test/shell_check_pass.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ 1 -lt 10 ]
+then
+    echo "This too shall pass"
+fi
